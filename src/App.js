7ade49f82fd2a2b3e3b554b57155f9822b5dbdc8
allomhg <allomhg@gmail.com>
2024-01-23 22:00:54 +0000

    Adding things in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import {Routes, Route, Link} from "react-router-dom";
+import { HomePage } from './pages/HomePage';
+import { ReservationPage } from './pages/ReservationPage';
 import { Header } from './components/Header/Header';
 import { Main } from './components/Main';
 import { Footer } from './components/Footer';
@@ -6,9 +9,17 @@ import { Footer } from './components/Footer';
 function App() {
   return (
     <>
-      <Header />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/reservation-page">Reservation</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/reservation-page" element={<ReservationPage />} />
+      </Routes>
+      {/* <Header />
       <Main />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
---
+++ src/pages/ReservationPage.js
@@ -0,0 +1,7 @@
+export const ReservationPage = () => {
+    return (
+        <div>
+            Booking Page
+        </div>
+    );
+}
