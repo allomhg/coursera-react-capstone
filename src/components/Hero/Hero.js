c2aeb67557de3cc4326b46ac7e211b792e06fe08
allomhg <allomhg@gmail.com>
2024-02-12 20:25:16 +0000

    Fixing CSS class names

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,18 +3,18 @@ import { Button } from '../../components/Button/Button';
 
 export const Hero = () => {
     return (
-        <section className={styles.hero} id="Hero">
-            <div className={styles.heroDiv}>
-                <h2 className={styles.heroHeading}>Little Lemon</h2>
+        <section className={styles.hero} id="hero">
+            <div className={styles["hero-div"]}>
+                <h2 className={styles["hero-heading"]}>Little Lemon</h2>
                 <h3>Chicago</h3>
-                <p className={styles.heroText}>
+                <p className={styles["hero-text"]}>
                     We are a family owned Mediterranean restaurant,
                     focused on traditional recipes served with a modern twist.
                 </p>
                 <Button txt="Reserve a Table" />
             </div>
-            <div className={styles.backDiv}></div>
-            <img src="/hero-image.jpg" alt="Itsa me! Mario!" id="hero-image" className={styles.heroImg}></img>
+            <div className={styles["hero-bg"]}></div>
+            <img src="/hero-image.jpg" alt="Itsa me! Mario!" id="hero-image" className={styles["hero-img"]}></img>
         </section>
     );
 }
--- src/components/Specials/Specials.js
+++ src/components/Specials/Specials.js
@@ -1,10 +1,11 @@
 import { Button } from '../../components/Button/Button';
 import { MenuCard } from '../MenuCard/MenuCard';
+import styles from './Specials.module.scss';
 
 export const Specials = () => {
     return (
-        <section>
-            <div>
+        <section className={styles.specialsContainer}>
+            <div className={styles.specialsHeader}>
                 <h2>This weeks specials!</h2>
                 <Button txt="Online Menu" />
             </div>
