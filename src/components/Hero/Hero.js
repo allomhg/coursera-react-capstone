526b635c51a4a48541769705271ead2be0375c06
allomhg <allomhg@gmail.com>
2024-02-12 20:11:21 +0000

    Updating Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,20 +1,20 @@
-import styles from './Hero.module.css';
+import styles from './Hero.module.scss';
 import { Button } from '../../components/Button/Button';
 
 export const Hero = () => {
     return (
-        <section className={styles.Hero} id="Hero">
-            <div>
-                <h2>Little Lemon</h2>
+        <section className={styles.hero} id="Hero">
+            <div className={styles.heroDiv}>
+                <h2 className={styles.heroHeading}>Little Lemon</h2>
                 <h3>Chicago</h3>
-                <p>
-                    Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <p className={styles.heroText}>
+                    We are a family owned Mediterranean restaurant,
+                    focused on traditional recipes served with a modern twist.
                 </p>
                 <Button txt="Reserve a Table" />
             </div>
-            <img src="/hero-image.jpg" alt="Itsa me! Mario!" id="hero-image" className={styles.HeroImg}></img>
+            <div className={styles.backDiv}></div>
+            <img src="/hero-image.jpg" alt="Itsa me! Mario!" id="hero-image" className={styles.heroImg}></img>
         </section>
     );
 }
